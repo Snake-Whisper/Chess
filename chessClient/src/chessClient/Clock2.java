b9eb2297c95125a4a29dfb30da26d5354bac8be9
snake-whisper <snake-whisper@web-utils.ml>
2019-02-11 18:03:51 +0100

    cleared Clock2

--- chessClient/src/chessClient/Clock2.java
+++ chessClient/src/chessClient/Clock2.java
@@ -1,32 +1,10 @@
 package chessClient;
 
-import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.*;
-import chessClient.Field.*;
+import javax.swing.JPanel;
 
+public class Clock2 extends JPanel {
 
-public class Clock2 extends JPanel{
-	
-	int chessBoarder = 50;
+	Clock2() {
 
-	Clock2(Container cp, int y){
-		/*JLabel clock = new JLabel();
-		clock.setBounds(8*Field.SIZE+Field.xChessBoarder+Field.chessBkDeep+chessBoarder,
-				y, (int) (Field.SIZE*1.5), (int) (Field.SIZE*1.5));
-		clock.setBackground(Color.blue);
-		clock.setOpaque(true);
-		
-		cp.add(clock);*/
-		
-		
-		
 	}
-	
-	/*public void paint(Graphics g, int y) {
-		Graphics2D g2d = (Graphics2D) g.create();
-		g2d.drawOval(8 * Field.SIZE+Field.xChessBoarder + Field.chessBkDeep+chessBoarder,
-				y, (int) (Field.SIZE*1.5), (int) (Field.SIZE*1.5));
-	}*/
 }
