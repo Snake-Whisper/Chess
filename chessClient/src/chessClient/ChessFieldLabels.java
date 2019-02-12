ed9158943cd70d7c755981cc0c22ba894b61b1a9
snake-whisper <snake-whisper@web-utils.ml>
2019-02-12 16:19:09 +0100

    Add boarder to labels

--- chessClient/src/chessClient/ChessFieldLabels.java
+++ chessClient/src/chessClient/ChessFieldLabels.java
@@ -11,13 +11,12 @@ import javax.swing.JPanel;
 public class ChessFieldLabels extends JPanel{
 	
 	Font ChessLabelFont = new Font("Serif", Font.BOLD, 24);
-	GridBagConstraints CONSTRAINTS;
+	GridBagConstraints CONSTRAINTS = new GridBagConstraints();
 
 	public ChessFieldLabels() {
 		
 		setLayout(new GridBagLayout());
 		JPanel chess = new Squares();
-		GridBagConstraints CONSTRAINTS = new GridBagConstraints();
 		CONSTRAINTS.fill = GridBagConstraints.HORIZONTAL;
 		CONSTRAINTS.anchor = GridBagConstraints.FIRST_LINE_START;
 		CONSTRAINTS.weightx = 0.5;
@@ -25,11 +24,16 @@ public class ChessFieldLabels extends JPanel{
 		CONSTRAINTS.gridx = 1;
 		CONSTRAINTS.gridy = 1;
 		CONSTRAINTS.gridwidth = 8;
+		CONSTRAINTS.gridwidth = 7;
+		CONSTRAINTS.gridwidth = 8;
 		CONSTRAINTS.gridheight = 8;
 		CONSTRAINTS.ipady = 0;
 		CONSTRAINTS.insets = new Insets(0, 0, 0, 0);
 		CONSTRAINTS.fill = GridBagConstraints.BOTH;
 		add(chess, CONSTRAINTS);
+		CONSTRAINTS.gridwidth = 7;
+		
+		generateChessLabels();
 		
 	}
 	
@@ -41,18 +45,27 @@ public class ChessFieldLabels extends JPanel{
 		CONSTRAINTS.gridheight = 1;
 		
 		JLabel[] yLabels = new JLabel[8];
+		
 		for (int i = 0; i < 8; i++) {
+			CONSTRAINTS.gridy = 0;
+			CONSTRAINTS.gridx = i+1;
+			CONSTRAINTS.insets = new Insets(0, 0, 5, 0);
 			xLabels[i] = new JLabel();
 			xLabels[i].setText(chars.charAt(i) + "");
 			xLabels[i].setHorizontalAlignment(JLabel.CENTER);
-			xLabels[i].setVerticalAlignment(JLabel.TOP);
+			xLabels[i].setVerticalAlignment(JLabel.BOTTOM);
 			xLabels[i].setFont(ChessLabelFont);
+			add(xLabels[i], CONSTRAINTS);
 
+			CONSTRAINTS.gridx = 0;
+			CONSTRAINTS.gridy = i+1;
+			CONSTRAINTS.insets = new Insets(0, 0, 0, 5);
 			yLabels[i] = new JLabel();
 			yLabels[i].setText(i + 1 + "");
-			yLabels[i].setHorizontalAlignment(JLabel.LEFT);
+			yLabels[i].setHorizontalAlignment(JLabel.RIGHT);
 			yLabels[i].setVerticalAlignment(JLabel.CENTER);
 			yLabels[i].setFont(ChessLabelFont);
+			add(yLabels[i], CONSTRAINTS);
 
 		}
 	}
